37732bafde738767f8056183e6d91d95090cb3bf
najak <kamilnaja@gmail.com>
2019-02-03 13:32:21 +0100

    Make search tile looks better #9

--- src/app/userChooseInfo/UserChooseInfo.component.ts
+++ src/app/userChooseInfo/UserChooseInfo.component.ts
@@ -3,7 +3,10 @@ import { WeatherService } from './UserChoseInfo.service';
 
 @Component({
     selector: 'app-user-choose-info',
-    templateUrl: './UserChooseInfo.html'
+    templateUrl: './UserChooseInfo.html',
+    styles: [`.message {
+        margin-top: 15px;
+    }`]
 })
 
 export class UserChooseInfoComponent implements OnInit {
@@ -11,13 +14,13 @@ export class UserChooseInfoComponent implements OnInit {
     @Input() showResults: boolean;
 
     private _weatherApiResponse: any;
-    private _listItems = [];
+    private result = [];
 
     constructor(private _weatherService: WeatherService) { }
 
     ngOnInit(): void {
         this._weatherService.listItems$.subscribe(t => {
-            this._listItems = t;
+            this.result = t;
         });
     }
 
