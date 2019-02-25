907f72e5ebdc17c9c829cbef3736a161808756eb
najak <kamilnaja@gmail.com>
2019-02-25 14:50:47 +0100

    added sh file and change route to https

--- src/app/Utils/Environment.prod.ts
+++ src/app/Utils/Environment.prod.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from '../interfaces/IEnvironment';
 
 export const environmentProd: IEnvironment = {
     production: false,
-    baseUrl: 'http://api.openweathermap.org/data/2.5/weather',
+    baseUrl: 'https://api.openweathermap.org/data/2.5/weather',
     apiKey: '19bdc82416a280ee0d7025c4ae606556',
     localStorageKey: 'weather',
     flagAddress: 'hello'
